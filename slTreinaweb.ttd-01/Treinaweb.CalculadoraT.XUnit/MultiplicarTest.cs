b4ce370e1d6cfc1e76e5e4ea8b1426f4281113d4
Alex Ribeiro <alexfariakof@gmail.com>
2019-07-21 10:53:57 -0300

    Implementaçã de Testes Multiplicação

---
+++ slTreinaweb.ttd-01/Treinaweb.CalculadoraT.XUnit/MultiplicarTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using twc = Treinaweb.Calculadora.Util;
+
+namespace Treinaweb.CalculadoraT.XUnit
+{
+    public class MultiplicarTest : IDisposable
+    {
+        private twc.Calculadora _calc;
+
+        public MultiplicarTest()
+        {
+            _calc = new twc.Calculadora();
+        }
+
+        public void Dispose()
+        {
+            _calc = null;
+        }
+
+        [Fact]
+        public void TestMultiplicar()
+        {
+            Assert.Equal(16, _calc.Mutiplicar(8, 2));
+        }
+
+        [Fact]
+        public void TestMultiplicarPrimeiroArgumentoAbaixoLimite()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                _calc = new twc.Calculadora(2);
+                _calc.Mutiplicar(1, 2);
+            });
+        }
+
+        [Fact]
+        public void TestMultiplicarSegundoArgumentoAbaixoLimite()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                _calc = new twc.Calculadora(2);
+                _calc.Mutiplicar(2, 1);
+            });
+        }
+
+    }
+}
