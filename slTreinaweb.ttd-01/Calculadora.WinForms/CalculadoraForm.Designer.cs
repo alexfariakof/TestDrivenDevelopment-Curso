1c47bba52a0e642b50c6cfb20342fa008618364a
Alex Ribeiro <alexfariakof@gmail.com>
2019-07-21 12:09:27 -0300

    Implementação de Inteface da Calculadora

---
+++ slTreinaweb.ttd-01/Calculadora.WinForms/CalculadoraForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Calculadora.WinForms
+{
+    partial class CalculadoraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPrimeiroArgumento = new System.Windows.Forms.TextBox();
+            this.txtSegundoArgumento = new System.Windows.Forms.TextBox();
+            this.cboOperacao = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primeiro Arguimento:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Segundo Arguimento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operação:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(90, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado:";
+            // 
+            // txtPrimeiroArgumento
+            // 
+            this.txtPrimeiroArgumento.Location = new System.Drawing.Point(182, 21);
+            this.txtPrimeiroArgumento.Name = "txtPrimeiroArgumento";
+            this.txtPrimeiroArgumento.Size = new System.Drawing.Size(267, 26);
+            this.txtPrimeiroArgumento.TabIndex = 4;
+            // 
+            // txtSegundoArgumento
+            // 
+            this.txtSegundoArgumento.Location = new System.Drawing.Point(182, 53);
+            this.txtSegundoArgumento.Name = "txtSegundoArgumento";
+            this.txtSegundoArgumento.Size = new System.Drawing.Size(267, 26);
+            this.txtSegundoArgumento.TabIndex = 5;
+            // 
+            // cboOperacao
+            // 
+            this.cboOperacao.FormattingEnabled = true;
+            this.cboOperacao.Items.AddRange(new object[] {
+            "Soma",
+            "Subtração",
+            "Multiplicação",
+            "Divisão"});
+            this.cboOperacao.Location = new System.Drawing.Point(182, 84);
+            this.cboOperacao.Name = "cboOperacao";
+            this.cboOperacao.Size = new System.Drawing.Size(266, 28);
+            this.cboOperacao.TabIndex = 6;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(182, 115);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(266, 26);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(355, 147);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(93, 36);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // CalculadoraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 244);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.cboOperacao);
+            this.Controls.Add(this.txtSegundoArgumento);
+            this.Controls.Add(this.txtPrimeiroArgumento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CalculadoraForm";
+            this.Text = "CalculadoraForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPrimeiroArgumento;
+        private System.Windows.Forms.TextBox txtSegundoArgumento;
+        private System.Windows.Forms.ComboBox cboOperacao;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
---
+++ slTreinaweb.ttd-01/Calculadora.WinForms/CalculadoraForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using twc =Treinaweb.Calculadora.Util;
+
+
+namespace Calculadora.WinForms
+{
+    public partial class CalculadoraForm : Form
+    {
+        public CalculadoraForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1, num2 = 0;
+                if (!int.TryParse(txtPrimeiroArgumento.Text, out num1))
+                {
+                    MessageBox.Show("O primeiro número é inválido."); 
+                }
+
+                if (!int.TryParse(txtSegundoArgumento.Text, out num2))
+                {
+                    MessageBox.Show("O primeiro número é inválido.");
+                }
+
+                twc.Calculadora calc = new twc.Calculadora();
+                switch(cboOperacao.SelectedIndex)
+                {
+                    case 0:
+                        txtResultado.Text = calc.Somar(num1, num2).ToString();
+                        break;
+                    case 1:
+                        txtResultado.Text = calc.Subtrair(num1, num2).ToString();
+                        break;
+                    case 2:
+                        txtResultado.Text = calc.Mutiplicar(num1, num2).ToString();
+                        break;
+                    case 3:
+                        txtResultado.Text = calc.Dividir(num1, num2).ToString();
+                        break;
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
